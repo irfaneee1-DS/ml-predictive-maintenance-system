de91cf13b6c2444874ed501161e4724d6dd5fdeb
Mohamed Irfan Ali <irfaneee1@gmail.com>
2026-09-23 04:51:39 +0200

    Update influx_connection.py

--- utils/influx_connection.py
+++ utils/influx_connection.py
@@ -93,7 +93,7 @@ def _get_latest_from_influxdb():
         "Wavelet_D3": latest["Wavelet_D3"],
         "Wavelet_D4": latest["Wavelet_D4"],
         "Label": latest["Label"],
-        time = latest.get("Time", "CSV fallback"),
+        "Time": latest["time"],
         "Source": "InfluxDB (live)"
     }
 
